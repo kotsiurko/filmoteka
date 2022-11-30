2511b8c0ff64d9df2eabe6c7ccda46f94119b940
Ksenia Kovalenko <ksushka.ua@gmail.com>
2022-11-30 15:38:46 +0200

    added theme switcher, btn to top

--- src/js/to-top.js
+++ src/js/to-top.js
@@ -1,7 +1,7 @@
 const backToTop = document.querySelector('.back_to_top');
 
 window.addEventListener('scroll', trackScroll);
-backToTop.addEventListener('click', backToTop);
+backToTop.addEventListener('click', goToTop);
 
 function trackScroll() {
   const scrolled = window.pageYOffset;
@@ -15,22 +15,6 @@ function trackScroll() {
   }
 }
 
-function onTop() {
-  if (window.pageYOffset > 0) {
-    window.scrollBy(0, -30);
-    setTimeout(onTop, 0);
-  }
+function goToTop() {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
 }
-
-// const topBtn = document.querySelector('.back_to_top');
-
-// function scrollFunction() {
-//   if (
-//     document.body.scrollTop > 100 ||
-//     document.documentElement.scrollTop > 100
-//   ) {
-//     topBtn.classList.add('back_to_top-show');
-//   } else {
-//     topBtn.classList.remove('back_to_top-show');
-//   }
-// }
