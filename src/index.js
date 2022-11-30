9b95d130dae89dacc3c5fe88af1c06712043787d
Ksenia Kovalenko <ksushka.ua@gmail.com>
2022-11-30 20:22:38 +0200

    fixed scss

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
+import './js/preloader';
 import './js/pagination';
 import './js/test-warning';
 import './js/api-service';
 import './js/home-page-loading';
-import './js/preloader';
 import './js/theme-switch';
 import './js/to-top';
-import './js/to-top';
-// import './js/preloader';
