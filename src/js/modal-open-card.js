2f503d2403826dfe9c0437dffb7825f98559c25c
Roman Kotsiurko <kotsiurko@gmail.com>
2022-12-02 00:40:52 +0200

    add number converter function

--- src/js/modal-open-card.js
+++ src/js/modal-open-card.js
@@ -9,26 +9,51 @@ const modalContainerEl = document.querySelector('.tablet-container');
 // слухач на батьківський UL карток
 modalOpenEl.addEventListener('click', onModalOpenClick);
 const movieDB = new MovieDB();
+
+// Функція відкриття модалки
 async function onModalOpenClick(event) {
   event.preventDefault();
   if (event.target.closest('li')) {
-    // console.log('Тицаю на ЛІ');
     modalEl.classList.remove('is-hidden');
     modalCloseEl.addEventListener('click', onModalCloseClick);
     backdropEl.addEventListener('click', onBackdropElClick);
     window.addEventListener('keydown', onEscBtnClick);
+
     const selectedMovie = event.target.closest('li');
     // console.log(selectedMovie);
     const FilmID = selectedMovie.dataset.movieid;
     // console.log(FilmID);
     try {
       const { data } = await movieDB.fetchMovieById(FilmID);
-      // console.log(data);
+
+      // Достукуємось до ЛокалСторедж і робимо перевірку на
+      // наявність фільму в списках доданих
+      // console.log(data.id);
+      const LS_WWATCHED_ARRAY = JSON.parse(localStorage.getItem("watched films"));
+      // console.log(LS_WWATCHED_ARRAY);
+
+
       renderFilmCard(data);
+
+      const foundFilm = LS_WWATCHED_ARRAY.find(el => el.id === data.id);
+
       const watchedBtnEl = document.querySelector(".watched");
+      if (foundFilm) {
+        watchedBtnEl.textContent = 'Remove from watched';
+        // тут має бути логіка видалення фільму
+        // витягнути масив об'єктів
+        // видалити foundFilm з цього масиву
+        // записати новий масив в ЛокалСторедж
+        // JSON.parse(localStorage.getItem("watched films")
+      }
+
+      // console.dir(watchedBtnEl);
+
+
       watchedBtnEl.addEventListener("click", onWatchedBtnElClick = () => {
         localStrgWriteWatched(data);
       });
+
       const addToQueueBtnEl = document.querySelector(".queue");
       console.log(addToQueueBtnEl);
       addToQueueBtnEl.addEventListener("click", onaddToQueueElClick = () => {
@@ -136,7 +161,7 @@ function localStrgWriteWatched(data) {
   filmGenres = prepareObject(data.genres);
 
   const watchedFilms = JSON.parse(localStorage.getItem(WATCHED_STORAGE_KEY)) || [];
-  const { poster_path, title, vote_average, vote_count, popularity, original_title, overview } = data;
+  const { poster_path, title, vote_average, vote_count, popularity, original_title, overview, release_date, id } = data;
   const filmData = {
     title: title,
     poster_path: poster_path,
@@ -146,6 +171,8 @@ function localStrgWriteWatched(data) {
     original_title: original_title,
     overview: overview,
     filmGenres: filmGenres,
+    release_date: release_date,
+    id: id,
   };
   watchedFilms.push(filmData);
   const stringifyData = JSON.stringify(watchedFilms);
@@ -155,7 +182,7 @@ function localStrgWriteWatched(data) {
 function localStrgWriteAddToQueue(data) {
   filmGenres = prepareObject(data.genres);
   const watchedFilms = JSON.parse(localStorage.getItem(QUEUE_STORAGE_KEY)) || []
-  const { poster_path, title, vote_average, vote_count, popularity, original_title, overview } = data;
+  const { poster_path, title, vote_average, vote_count, popularity, original_title, overview, release_date, id } = data;
   const filmData = {
     title: title,
     poster_path: poster_path,
@@ -165,6 +192,8 @@ function localStrgWriteAddToQueue(data) {
     original_title: original_title,
     overview: overview,
     filmGenres: filmGenres,
+    release_date: release_date,
+    id: id,
   };
 
   watchedFilms.push(filmData);
@@ -176,3 +205,34 @@ function localStrgWriteAddToQueue(data) {
 function localStorageRemove(data) {
   localStorage.removeItem(data)
 }
+
+
+
+// function onModalOpenClick(event) {
+//   event.preventDefault();
+//   if (event.target.closest('li')) {
+//     modalEl.classList.remove('is-hidden');
+//     modalCloseEl.addEventListener('click', onModalCloseClick);
+//     backdropEl.addEventListener('click', onBackdropElClick);
+//     window.addEventListener('keydown', onEscBtnClick);
+//   } else {
+//     alert('Please click on film image');
+//   }
+//   return;
+// }
+// function onModalCloseClick() {
+//   modalEl.classList.add('is-hidden');
+//   modalCloseEl.removeEventListener('click', onModalCloseClick);
+//   backdropEl.removeEventListener('click', onBackdropElClick);
+//   window.removeEventListener('keydown', onEscBtnClick);
+// }
+// function onBackdropElClick(event) {
+//   if (event.target === backdropEl) {
+//     onModalCloseClick();
+//   }
+// }
+// function onEscBtnClick(event) {
+//   if (event.code === 'Escape') {
+//     onModalCloseClick();
+//   }
+// }
