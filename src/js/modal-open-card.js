72ef2c6e80f4f73ba1b179c14966231e3075766e
Roman Kotsiurko <kotsiurko@gmail.com>
2022-12-03 04:06:42 +0200

    add Queue btn functionality
    add Queue btn functionality on home page

--- src/js/modal-open-card.js
+++ src/js/modal-open-card.js
@@ -1,5 +1,6 @@
 import { MovieDB } from './api-service';
-import { getMovieDetails } from './home-page-loading';
+import { numberConverter } from './prepare-number';
+// import { getMovieDetails } from './home-page-loading';
 const modalOpenEl = document.querySelector('[data-modal-open]');
 const modalCloseEl = document.querySelector('[data-modal-close]');
 const modalEl = document.querySelector('[data-modal]');
@@ -8,206 +9,17 @@ const modalContainerEl = document.querySelector('.tablet-container');
 const body = document.querySelector('body');
 
 const WATCHED_STORAGE_KEY = "watched films";
-const QUEUE_STORAGE_KEY = "films in queue"
+const QUEUE_STORAGE_KEY = "films in queue";
 
 // слухач на батьківський UL карток
 modalOpenEl.addEventListener('click', onModalOpenClick);
 
 const movieDB = new MovieDB();
 
-// Функція відкриття модалки
-async function onModalOpenClick(event) {
-  event.preventDefault();
-  if (event.target.closest('li')) {
-    modalEl.classList.remove('is-hidden');
-    modalCloseEl.addEventListener('click', onModalCloseClick);
-    backdropEl.addEventListener('click', onBackdropElClick);
-    window.addEventListener('keydown', onEscBtnClick);
 
-    const selectedMovie = event.target.closest('li');
-    // console.log(selectedMovie);
-    const FilmID = selectedMovie.dataset.movieid;
-    // console.log(FilmID);
-
-
-
-    try {
-      const { data } = await movieDB.fetchMovieById(FilmID);
-      // console.log(data);
-      renderFilmCard(data);
-      body.classList.add('noScroll');
-      const watchedBtnEl = document.getElementById("watched");
-
-      // зчитування інформації з дата-атрибуту кнопки
-      const modalFilmIdEl = document.querySelector('[data-filmid]');
-      const modalFilmId = modalFilmIdEl.dataset.filmid;
-
-      // Перезапис атрибута кнопки значенням data-watched="false/true"
-      // в залежності від того, чи є фільм в LS
-
-      // Сценарій, коли якісь фільми вже є в ЛС
-      if (localStorage.getItem('watched films') !== null) {
-
-        // Пошук фільму в масиві збережених в ЛС
-        const LS_WWATCHED_ARRAY = JSON.parse(localStorage.getItem("watched films"));
-        // Витягуємо індекс фільму з масиву збережених в ЛокалСторедж
-        let foundFilmIndex = LS_WWATCHED_ARRAY.findIndex(el => el.id === Number(modalFilmId));
-        console.log(foundFilmIndex);
-
-        // якщо фільму немає в ЛС
-        if (foundFilmIndex === -1) {
-          console.log("Film is NOT in LS");
-          watchedBtnEl.dataset.watched = 'false';
-          watchedBtnEl.textContent = 'add to Watche';
-          watchedBtnEl.addEventListener('click', onBtnWatchedClick)
-          return;
-        }
-        // якщо фільм є в ЛС
-        if (foundFilmIndex + 1) {
-          console.log("Film is in LS");
-          watchedBtnEl.dataset.watched = 'true';
-          watchedBtnEl.textContent = 'Remove from Watched';
-          watchedBtnEl.addEventListener('click', onBtnWatchedClick)
-          return;
-        }
-        return;
-      }
-
-      // Сценарій, коли ЛС повністю пустий
-      if (localStorage.getItem('watched films') === null) {
-        // Випадок, коли ЛС чистий і ми все одно можемо записати фільм в ЛС
-        watchedBtnEl.dataset.watched = 'false';
-        watchedBtnEl.addEventListener('click', onBtnWatchedClick)
-        return;
-      }
-
-
-
-
-
-      // обробник події кліку по кнопці
-      // перевірка по атрибуту data-watched="false"
-      // з подальшим записом чи видаленням фільму з ЛС
-      // та перемальовкою кнопки та її атрибута
-      //
-
-
-      // function onBtnClick() {
-      //   console.log("Подія обробника кліку");
-      //   // onBtnWatchedClick();
-      //   // або так
-      //   // onBtnWatchedClick;
-      // }
-
-
-      function onBtnWatchedClick() {
-        // якщо data-watched="false", то
-        if (watchedBtnEl.dataset.watched === 'false') {
-          //	викликаю функцію запису об'єкта в ЛС;
-          console.log("Тіпа додав фільм в ЛС");
-          console.log(data);
-          addFilmToLS(data);
-          // Перемальовую кнопку
-          watchedBtnEl.dataset.watched = 'true';
-          watchedBtnEl.textContent = 'Remove from Watched';
-
-          // ?? Можливо викликаю колбек-функцію, яка викликає сама-себе
-          modalFilmIdEl.addEventListener('click', () => {
-            onBtnWatchedClick;
-          })
-          return;
-        }
-        // якщо data-watched="true", то
-        if (watchedBtnEl.dataset.watched === 'true') {
-          //	викликаю функцію видалення об'єкта з ЛС:
-          console.log("Тіпа видалив фільм з ЛС");
-          console.log(modalFilmId);
-          removeFilmFromLS(modalFilmId);
-          // Перемальовую кнопку
-          watchedBtnEl.dataset.watched = 'false';
-          watchedBtnEl.textContent = 'Add to Watch';
-
-          //?? Можливо викликаю колбек-функцію, яка викликає сама-себе
-          modalFilmIdEl.addEventListener('click', () => {
-            onBtnWatchedClick;
-          })
-          return;
-        }
-      }
-    } catch (err) {
-      console.log(err);
-    }
-  } else {
-    alert('Please click on film image');
-  }
-  return;
-}
-
-
-function onModalCloseClick() {
-  modalEl.classList.add('is-hidden');
-  body.classList.remove('noScroll');
-  modalCloseEl.removeEventListener('click', onModalCloseClick);
-  backdropEl.removeEventListener('click', onBackdropElClick);
-  window.removeEventListener('keydown', onEscBtnClick);
-}
-
-
-
-
-
-
-
-// // ======================================================
-// // функція видалення фільму з ЛС
-// // ======================================================
-function removeFilmFromLS(filmId) {
-
-  // filmId - текстова стрічка id фільму із АПІ
-  console.log(filmId);
-  const LS_WWATCHED_ARRAY = JSON.parse(localStorage.getItem("watched films"));
-  let foundFilmIndex = LS_WWATCHED_ARRAY.findIndex(el => el.id === Number(filmId));
-  // console.log(foundFilmIndex);
-
-  // видалити знайдений фільм з цього масиву за індексом
-  LS_WWATCHED_ARRAY.splice(foundFilmIndex, 1);
-  console.log(LS_WWATCHED_ARRAY);
-  // записати новий масив в ЛокалСторедж
-  localStorage.setItem("watched films", JSON.stringify(LS_WWATCHED_ARRAY));
-
-  console.log("Фільм видалений");
-}
-// // ======================================================
-
-
-// // ======================================================
-// // Функція додавання фільму в ЛС
-// // ======================================================
-function addFilmToLS(film) {
-  console.log(film);
-  localStrgWriteWatched(film);
-  console.log("Фільм доданий");
-}
-// // ======================================================
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// ======================================================
+// Функції-обробники закриття/відериття модального вікна
+// ======================================================
 function onBackdropElClick(event) {
   if (event.target === backdropEl) {
     body.classList.remove('noScroll');
@@ -221,6 +33,186 @@ function onEscBtnClick(event) {
     body.classList.remove('noScroll');
   }
 }
+
+function onModalCloseClick() {
+  modalEl.classList.add('is-hidden');
+  body.classList.remove('noScroll');
+  modalCloseEl.removeEventListener('click', onModalCloseClick);
+  backdropEl.removeEventListener('click', onBackdropElClick);
+  window.removeEventListener('keydown', onEscBtnClick);
+}
+
+// Головна функція-обробник появи модального вікна
+async function onModalOpenClick(event) {
+  event.preventDefault();
+  if (event.target.closest('li')) {
+    modalEl.classList.remove('is-hidden');
+    modalCloseEl.addEventListener('click', onModalCloseClick);
+    backdropEl.addEventListener('click', onBackdropElClick);
+    window.addEventListener('keydown', onEscBtnClick);
+
+    const selectedMovie = event.target.closest('li');
+    const FilmID = selectedMovie.dataset.movieid;
+
+    try {
+      const { data } = await movieDB.fetchMovieById(FilmID);
+      renderFilmCard(data);
+      body.classList.add('noScroll');
+      const watchedBtnEl = document.getElementById("watched");
+      const queuedBtnEl = document.getElementById("queued");
+
+      // зчитування інформації з дата-атрибуту кнопки
+      const modalFilmIdEl = document.querySelector('[data-filmid]');
+      const modalFilmId = modalFilmIdEl.dataset.filmid;
+
+
+      // ================================================
+      // Блок роботи із Переглянутими фільмами та ЛС
+      // ================================================
+      // Далі йде логіка перезапису атрибута data-watched="false/true"
+      // в залежності від того, чи є фільм в Переглянутих в LS
+
+      // Сценарій, коли ЛС p Переглянутими повністю пустий
+      if (localStorage.getItem('watched films') === null) {
+        // Випадок, коли ЛС чистий і ми все одно можемо записати фільм в ЛС
+        watchedBtnEl.dataset.watched = 'false';
+        watchedBtnEl.addEventListener('click', onBtnWatchedClick);
+      }
+
+      // Сценарій, коли якісь фільми вже є в ЛС
+      if (localStorage.getItem('watched films') !== null) {
+
+        // Пошук фільму в масиві збережених в ЛС
+        const LS_WWATCHED_ARRAY = JSON.parse(localStorage.getItem("watched films"));
+        // Витягуємо індекс фільму з масиву збережених в ЛокалСторедж
+        let foundFilmIndex = LS_WWATCHED_ARRAY.findIndex(el => el.id === Number(modalFilmId));
+
+        // якщо фільму немає в ЛС
+        if (foundFilmIndex === -1) {
+          watchedBtnEl.dataset.watched = 'false';
+          watchedBtnEl.textContent = 'add to Watche';
+          watchedBtnEl.addEventListener('click', onBtnWatchedClick);
+        }
+        // якщо фільм є в ЛС
+        if (foundFilmIndex + 1) {
+          watchedBtnEl.dataset.watched = 'true';
+          watchedBtnEl.textContent = 'Remove from Watched';
+          watchedBtnEl.addEventListener('click', onBtnWatchedClick)
+        }
+      }
+
+      // обробник події кліку по кнопці
+      // перевірка по атрибуту data-watched="false"
+      // з подальшим записом чи видаленням фільму з ЛС
+      // та перемальовкою кнопки та її атрибута
+      function onBtnWatchedClick() {
+        // якщо data-watched="false", то
+        if (watchedBtnEl.dataset.watched === 'false') {
+          //	викликаю функцію запису об'єкта в ЛС;
+          addFilmToLS(data, "watched films");
+          // Перемальовую кнопку
+          watchedBtnEl.dataset.watched = 'true';
+          watchedBtnEl.textContent = 'Remove from Watched';
+
+          // Викликаю колбек-функцію, яка викликає сама-себе
+          modalFilmIdEl.addEventListener('click', () => {
+            onBtnWatchedClick;
+          })
+          return;
+        }
+        // якщо data-watched="true", то
+        if (watchedBtnEl.dataset.watched === 'true') {
+          //	викликаю функцію видалення об'єкта з ЛС:
+          removeFilmFromLS(modalFilmId, "watched films");
+          // Перемальовую кнопку
+          watchedBtnEl.dataset.watched = 'false';
+          watchedBtnEl.textContent = 'Add to Watch';
+
+          // Викликаю колбек-функцію, яка викликає сама-себе
+          modalFilmIdEl.addEventListener('click', () => {
+            onBtnWatchedClick;
+          })
+          return;
+        }
+      }
+      // ================================================
+
+
+
+      // ================================================
+      // Блок роботи із фільмами в Черзі та ЛС
+      // ================================================
+      // Далі йде логіка перезапису атрибута data-queued="false/true"
+      // в залежності від того, чи є фільм в Черзі в LS
+
+      // Сценарій, коли ЛС повністю пустий
+      if (localStorage.getItem("films in queue") === null) {
+        // Випадок, коли ЛС з Чергою чистий і ми все одно можемо записати фільм в ЛС
+        queuedBtnEl.dataset.queued = 'false';
+        queuedBtnEl.addEventListener('click', onBtnQueuedClick)
+        return;
+      }
+
+      if (localStorage.getItem("films in queue") !== null) {
+        // Пошук фільму в масиві збережених в ЛС
+        const LS_ARRAY = JSON.parse(localStorage.getItem("films in queue"));
+        // Витягуємо індекс фільму з масиву збережених в ЛокалСторедж
+        let foundFilmIndex = LS_ARRAY.findIndex(el => el.id === Number(modalFilmId));
+
+        // якщо фільму немає в ЛС
+        if (foundFilmIndex === -1) {
+          queuedBtnEl.dataset.queued = 'false';
+          queuedBtnEl.textContent = 'add to queue';
+          queuedBtnEl.addEventListener('click', onBtnQueuedClick)
+        }
+        // якщо фільм є в ЛС
+        if (foundFilmIndex + 1) {
+          queuedBtnEl.dataset.queued = 'true';
+          queuedBtnEl.textContent = 'Remove from Queue';
+          queuedBtnEl.addEventListener('click', onBtnQueuedClick)
+        }
+      }
+
+      function onBtnQueuedClick() {
+        // якщо data-queued="false", то
+        if (queuedBtnEl.dataset.queued === 'false') {
+          //	викликаю функцію запису об'єкта в ЛС;
+          addFilmToLS(data, "films in queue");
+          // Перемальовую кнопку
+          queuedBtnEl.dataset.queued = 'true';
+          queuedBtnEl.textContent = 'Remove from Queue';
+
+          // Викликаю колбек-функцію, яка викликає сама-себе
+          modalFilmIdEl.addEventListener('click', () => {
+            onBtnQueuedClick;
+          })
+          return;
+        }
+        // якщо data-queued="true", то
+        if (queuedBtnEl.dataset.queued === 'true') {
+          //	викликаю функцію видалення об'єкта з ЛС:
+          removeFilmFromLS(modalFilmId, "films in queue");
+          // Перемальовую кнопку
+          queuedBtnEl.dataset.queued = 'false';
+          queuedBtnEl.textContent = 'Add to Queue';
+
+          // Викликаю колбек-функцію, яка викликає сама-себе
+          modalFilmIdEl.addEventListener('click', () => {
+            onBtnQueuedClick;
+          })
+          return;
+        }
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  } else {
+    alert('Please click on film image');
+  }
+  return;
+}
+// ======================================================
+
 
 
 function prepareObject(array) {
@@ -237,6 +229,7 @@ function prepareObject(array) {
 }
 
 
+
 function renderFilmCard(data) {
   const {
     poster_path,
@@ -247,9 +240,12 @@ function renderFilmCard(data) {
     original_title,
     overview,
     id,
+    genres
   } = data;
 
-  let filmGenres = prepareObject(data.genres);
+  let filmGenres = prepareObject(genres);
+  let filmVotingAverage = numberConverter(vote_average);
+  let cuttedPopularity = numberConverter(popularity);
 
 
   const markup = `
@@ -260,13 +256,13 @@ function renderFilmCard(data) {
         <ul class="film-info__list">
           <li class="film-info__item">
             <p class="film-info__item--el">Vote / Votes</p>
-            <span class="film-info__vote">${vote_average}</span>
+            <span class="film-info__vote">${filmVotingAverage}</span>
             <span> &nbsp;/&nbsp;</span>
             <span class="film-info__params">${vote_count}</span>
           </li>
           <li class="film-info__item">
             <p class="film-info__item--el">Popularity</p>
-            <span class="film-info__params">${popularity}</span>
+            <span class="film-info__params">${cuttedPopularity}</span>
           </li>
           <li class="film-info__item">
             <p class="film-info__item--el">Original Title</p>
@@ -283,40 +279,21 @@ function renderFilmCard(data) {
         </p>
         <div class="btn-container">
         <button class="button button__orange" id="watched" data-watched="false">add to Watched</button>
-        <button class="button button__transparent">add to queue</button>
+        <button class="button button__transparent" id="queued" data-queued="false">add to queue</button>
       </div>
         </div>`;
   modalContainerEl.innerHTML = markup;
 }
 
-
-function localStrgWriteWatched(data) {
-  console.log(data);
-
+// ======================================================================
+// УНІВЕРСАЛЬНА Функція ДОДАВАННЯ ФІЛЬМУ
+// в Переглянуті / Чергу в ЛС
+// data - текстова стрічка id фільму із АПІ
+// key - ключ ("watched films" / "films in queue")
+// ======================================================================
+function addFilmToLS(data, key) {
   let filmGenres = prepareObject(data.genres);
-
-  const watchedFilms = JSON.parse(localStorage.getItem(WATCHED_STORAGE_KEY)) || [];
-  const { poster_path, title, vote_average, vote_count, popularity, original_title, overview, release_date, id } = data;
-  const filmData = {
-    title: title,
-    poster_path: poster_path,
-    vote_average: vote_average,
-    vote_count: vote_count,
-    popularity: popularity,
-    original_title: original_title,
-    overview: overview,
-    filmGenres: filmGenres,
-    release_date: release_date,
-    id: id,
-  };
-  watchedFilms.push(filmData);
-  const stringifyData = JSON.stringify(watchedFilms);
-  localStorage.setItem(WATCHED_STORAGE_KEY, stringifyData);
-}
-
-function localStrgWriteAddToQueue(data) {
-  let filmGenres = prepareObject(data.genres);
-  const watchedFilms = JSON.parse(localStorage.getItem(QUEUE_STORAGE_KEY)) || []
+  const filmsArray = JSON.parse(localStorage.getItem(key)) || []
   const { poster_path, title, vote_average, vote_count, popularity, original_title, overview, release_date, id } = data;
   const filmData = {
     title: title,
@@ -331,41 +308,28 @@ function localStrgWriteAddToQueue(data) {
     id: id,
   };
 
-  watchedFilms.push(filmData);
-  const stringifyData = JSON.stringify(watchedFilms);
-  localStorage.setItem(QUEUE_STORAGE_KEY, stringifyData);
+  filmsArray.push(filmData);
+  const stringifyData = JSON.stringify(filmsArray);
+  localStorage.setItem(key, stringifyData);
 }
+// // ======================================================
 
 
-// function localStorageRemove(data) {
-//   localStorage.removeItem(data)
-// }
 
-// function onModalOpenClick(event) {
-//   event.preventDefault();
-//   if (event.target.closest('li')) {
-//     modalEl.classList.remove('is-hidden');
-//     modalCloseEl.addEventListener('click', onModalCloseClick);
-//     backdropEl.addEventListener('click', onBackdropElClick);
-//     window.addEventListener('keydown', onEscBtnClick);
-//   } else {
-//     alert('Please click on film image');
-//   }
-//   return;
-// }
-// function onModalCloseClick() {
-//   modalEl.classList.add('is-hidden');
-//   modalCloseEl.removeEventListener('click', onModalCloseClick);
-//   backdropEl.removeEventListener('click', onBackdropElClick);
-//   window.removeEventListener('keydown', onEscBtnClick);
-// }
-// function onBackdropElClick(event) {
-//   if (event.target === backdropEl) {
-//     onModalCloseClick();
-//   }
-// }
-// function onEscBtnClick(event) {
-//   if (event.code === 'Escape') {
-//     onModalCloseClick();
-//   }
-// }
+// // ======================================================
+// Універсальна функція ВИДАЛЕННЯ ФІЛЬМУ (filmId)
+// з Переглянути чи черги із ЛС
+// filmId - текстова стрічка id фільму із АПІ
+// key - ключ ("watched films" / "films in queue")
+// // ======================================================
+function removeFilmFromLS(filmId, key) {
+
+  const LS_ARRAY = JSON.parse(localStorage.getItem(key));
+  let foundFilmIndex = LS_ARRAY.findIndex(el => el.id === Number(filmId));
+
+  // видалити знайдений фільм з цього масиву за індексом
+  LS_ARRAY.splice(foundFilmIndex, 1);
+  // записати новий масив в ЛокалСторедж
+  localStorage.setItem(key, JSON.stringify(LS_ARRAY));
+}
+// // ======================================================
